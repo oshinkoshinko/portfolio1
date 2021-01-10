9aacddbc86e92ce495c34e7636159a240c9bf6a6
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-10 07:33:32 +0000

    Make Favorite btn

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,17 @@
 class FavoritesController < ApplicationController
 
   def create
+    now = Now.find(params[:now_id])
+    favorite = current_user.favorites.new(now_id: now.id)
+    favorite.save
+    redirect_to request.referer
   end
 
   def destroy
+    now = Now.find(params[:now_id])
+    favorite = current_user.favorites.find_by(now_id: now.id)
+    favorite.destroy
+    redirect_to request.referer
   end
 
 end

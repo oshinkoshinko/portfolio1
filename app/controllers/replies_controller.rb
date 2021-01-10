6db99d6ed19e6a90cf832c6abfea9a417900c09e
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-10 09:05:11 +0000

    Update Reply page reply function

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,6 +1,11 @@
 class RepliesController < ApplicationController
 
   def create
+    @now = Now.find(params[:now_id])
+    @reply = current_user.replies.new(reply_params)
+    @reply.now_id = @now.id
+    @reply.save
+    redirect_to request.referer
   end
 
   def destroy
@@ -8,6 +13,11 @@ class RepliesController < ApplicationController
 
   def show
     @now = Now.find(params[:id])
+    @reply_new = Reply.new
+  end
+
+  def reply_params
+    params.require(:reply).permit(:reply)
   end
 
 end

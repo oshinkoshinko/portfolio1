e0c71b7fe20e5973b1f8d236ea33e8f3fafac2ba
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-10 05:41:33 +0000

    Mkae profile edit form

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Refile.secret_key = 'eba60a25a414666d0e3c76a684cb68c7d3a889518ac9ba9d12f1be8602e13545fb484c2dd03f97d1caf378742183c9f98c11a49e1a0de080e468e0a5d4089ea2'
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'homes/about'
 
   resources :nows, only: [:new, :create, :index, :show, :edit, :destroy]
-  resources :users, only: [:index, :create, :show, :edit, :destroy]
+  resources :users, only: [:index, :create, :show, :edit, :update, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

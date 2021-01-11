4f42dc7573dedaf19bbc2f1747bf147135f3aaed
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-11 08:08:43 +0000

    Done ajax of favorite btn

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,15 @@
 class FavoritesController < ApplicationController
 
   def create
-    now = Now.find(params[:now_id])
-    favorite = current_user.favorites.new(now_id: now.id)
+    @now = Now.find(params[:now_id])
+    favorite = current_user.favorites.new(now_id: @now.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
-    now = Now.find(params[:now_id])
-    favorite = current_user.favorites.find_by(now_id: now.id)
+    @now = Now.find(params[:now_id])
+    favorite = current_user.favorites.find_by(now_id: @now.id)
     favorite.destroy
-    redirect_to request.referer
   end
 
 end

ff46f363c448c73beea46bb19b102d60afd4ab24
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-11 09:11:20 +0000

    Update follow partial code

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,14 @@
 class RelationshipsController < ApplicationController
 
   def create
+    #ajax用に@user追加
+    #@user = User.find(params[:id])
     current_user.follow(params[:user_id])
     redirect_to request.referer
   end
 
   def destroy
+    #@user = User.find(params[:id])
     current_user.unfollow(params[:user_id])
     redirect_to request.referer
   end

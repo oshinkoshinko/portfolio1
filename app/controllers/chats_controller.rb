539a60c964b13c129e2d9d5857028b1864af3024
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-16 16:13:55 +0000

    Update Chats Controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,12 +1,23 @@
 class ChatsController < ApplicationController
 
   def show
+    @chat_new = Chat.new
+    @chats = Chat.all
+
   end
 
   def create
+    @chat = Chat.new(chat_params)
+    @chat.user_id = current_user.id
+    @chat.save
+    redirect_to request.referer
   end
 
   def destroy
+  end
+
+  def chat_params
+    params.require(:chat).permit(:message, :room_id)
   end
 
 end

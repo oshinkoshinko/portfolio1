d95bf6dfe7f6dd1b49b91a786c59cbf835b4216d
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-17 05:44:05 +0000

    Update ChatsController show action

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,14 +1,24 @@
 class ChatsController < ApplicationController
 
   def show
-    @chat_new = Chat.new
-    @chats = Chat.all
+    @user = User.find(params[:id])
+    rooms = current_user.user_rooms.pluck(:room_id)
+    user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
 
+    unless user_rooms.nil?
+      @room = user_rooms.room
+    else
+      @room = Room.new
+      @room.save
+      UserRoom.create(user_id: current_user.id, room_id: @room.id)
+      UserRoom.create(user_id: @user.id, room_id: @room.id)
+    end
+    @chats = @room.chats
+    @chat = Chat.new(room_id: @room.id)
   end
 
   def create
-    @chat = Chat.new(chat_params)
-    @chat.user_id = current_user.id
+    @chat = current_user.chats.new(chat_params)
     @chat.save
     redirect_to request.referer
   end

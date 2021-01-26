050b7eebbdd7622efac96297a6abbae56a806a05
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-26 10:11:46 +0000

    Adjust indent of nows/index

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,9 @@
 class RoomsController < ApplicationController
 
   def index
+    binding.pry
     @user = User.find(params[:id])
-    rooms = current_user/user_rooms.pluck(:room_id)
+    rooms = current_user.user_rooms.pluck(:room_id)
   end
 
 end

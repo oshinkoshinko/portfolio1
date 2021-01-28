f7b9f844f2146a5bccae23ee1e7f8d73413fb9df
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-28 02:36:13 +0000

    Add price form events/new

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
       get :detail
     end
   end
+  
   get '/rooms/' => 'rooms#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

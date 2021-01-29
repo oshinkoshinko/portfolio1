0327f6cb06e2709f105066e362b43abf2d5215c0
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-29 13:14:54 +0000

    Change background-color and color in all page

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -1,10 +1,16 @@
 class EventUsersController < ApplicationController
- 
-  def new
-  end
-  
+
   def create
+    @event_user = EventUser.find_by(params[:event_id])
+    #@event_user = EventUser.find(event_user_params)
+    #binding.pry
+    @event_user.create
+    redirect_to request.referer
+    binding.pry
   end
-  
-  
+
+  def event_user_params
+    params.require(:event_user).permit(:user_id, :event_id, :payment_method)
+  end
+
 end
